e77f9b951213a7d6c1984ea4a3d7fbc2511ea6b6
George Czabania <george.czabania@sparkventures.co.nz>
2015-10-29 22:46:10 +1300

    Switch to ES6 and stop using rewire

--- test/test.js
+++ test/test.js
@@ -1,54 +1,49 @@
-var assert = require('assert');
-var unwire = require('../lib');
+import assert from 'assert';
+import unwire, {flush, flushAll} from '../lib';
 
-var mockFs = {
-  readFileSync: function () {
+function mockReadFile (original) {
+  return function () {
     return 'some content';
-  }
+  };
 };
-
 
 describe('unwire', function () {
 
-  it('should replace readFile in ./source/main', function () {
-
-    var main = require('./source/main');
+  it('should fail by default', function () {
+    const main = require('./source/main');
 
     // by default, main() should throw an error
     assert.throws(main, /no such file or directory/);
 
     // remove ./source/main from require.cache 
-    unwire.flush('./source/main');
+    flush('./source/main');
+  });
 
+  it('should replace readFile in ./source/main', function () {
     // unwire readfile
     // https://www.npmjs.org/package/rewire => details on how to use rewire
-    var readFile = unwire('./source/readFile');
-    readFile.__set__('fs', mockFs);
+    const readFile = unwire('./source/readFile', mockReadFile);
 
     // load main
     // note that it now uses the rewired version of readFile
-    main = require('./source/main');
+    flush('./source/main');
+    const main = require('./source/main');
 
     assert(main() === 'some content');
 
+    flush('./source/main');
   });
 
   it('should reuse the unwired version', function () {
-
-    // flush everything
-    unwire.flush();
-
-    var a = unwire('./source/readFile');
-    var b = unwire('./source/readFile');
+    const a = unwire('./source/readFile', mockReadFile);
+    const b = unwire('./source/readFile', mockReadFile);
 
     assert.equal(a, b);
 
-    a.__unwire__();
+    flushAll();
+    const c = unwire('./source/readFile', mockReadFile);
 
-    var c = unwire('./source/readFile');
     assert.notEqual(a, c);
     assert.notEqual(b, c);
-
   });
-
 });
